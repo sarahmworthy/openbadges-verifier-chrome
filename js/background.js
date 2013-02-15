8a12016218953b5daa1a6a8be20fcf7b02e05a20
eldwin <eldwin_@hotmail.com>
2013-02-15 16:01:56 +0800

    removed useless comment

--- js/background.js
+++ js/background.js
@@ -6,15 +6,4 @@
 	chrome.browserAction.onClicked.addListener(function (tab) {
 		chrome.tabs.sendMessage(tab.id, '');
 	});
-
-/*
-	OpenBadges.Displayer.getAll('eldwin.liew@gmail.com',
-		function (profile) {
-			console.log(profile);
-		},
-		function (error) {
-			console.log(error);
-		}
-	);
-*/
 }());
